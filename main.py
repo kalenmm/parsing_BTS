6a4d93b6af0ad14bd63600f354d5ead0bcaf92dc
Kalen Meiirzhan <kalen.mm@mail.ru>
2022-07-05 19:48:20 +0600

    first commit

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from config import user, host, password, db_name
 import psycopg2
 from selenium.webdriver.common.by import By
 
-# Creation of datbase
+# Uncomment it for creation table
+# Creation of database
 # try:
 #     connection = psycopg2.connect(
 #         host=host,
@@ -17,50 +18,49 @@ from selenium.webdriver.common.by import By
 #         )
 #         print(f"Server version: {cursor.fetchone()}")
 #
-#     # with connection.cursor() as cursor:
-#     #     cursor.execute(
-#     #         """CREATE TABLE parser(
-#     #             id serial primary key,
-#     #             job_name varchar(80),
-#     #             company_name varchar(80),
-#     #             location varchar(80),
-#     #             description varchar(360));"""
-#     #     )
-#     #     connection.commit()
-#     #     print("[Table created successfully]")
+#     with connection.cursor() as cursor:
+#         cursor.execute(
+#             """CREATE TABLE parser(
+#                 id serial primary key,
+#                 job_name varchar(1801),
+#                 company_name varchar(1802),
+#                 description varchar(29000));"""
+#         )
+#         connection.commit()
+#         print("[Table created successfully]")
 #     connection.close()
 # except Exception as _ex:
 #     print("[INFO] error while working with PostgreSQL", _ex)
 
 
-class ProgParser(object):
-
-    def __init__(self):
-        pass
-
-    def parse(self):
-        pass
-
-
+# main function
 def main():
     driver = webdriver.Chrome()
     driver.get(
         'https://hh.kz/search/vacancy?area=160&search_field=name&search_field=company_name&search_field=description&text=python&from=suggest_post&hhtmFrom=vacancy_search_list')
+    # Testcase for running program
     # main_element = driver.find_element(by=By.CLASS_NAME, value='vacancy-serp-item')
     # print(main_element)
     job_name = driver.find_elements(by=By.TAG_NAME, value="h3")
     company_name = driver.find_elements(by=By.CLASS_NAME, value="vacancy-serp-item__meta-info-company")
-    description = driver.find_elements(by=By.CLASS_NAME, value="bloko-text_no-top-indent")
-    ma_dict = {}
+    description = driver.find_elements(by=By.CLASS_NAME, value="g-user-content")
+    data1 = [120]
+    data2 = [120]
+    data3 = [120]
+    # For loop for checking progress
     for c in description:
-        print("Description - ", c.text)
-        ma_dict["Desc"] = c.text
+        data1.append(c.text)
     for j in company_name:
-        print("Company name - ", j.text)
+        data2.append(j.text)
     for i in job_name:
-        print(i.text)
+        data3.append(i.text)
+    print(len(data1))
+    print(len(data2))
+    print(len(data3))
+    data = [tuple(data1), tuple(data2), tuple(data3)]
 
-# Inserting values
+
+# Inserting values into database
     try:
         connection = psycopg2.connect(
             host=host,
@@ -70,26 +70,20 @@ def main():
         )
 
         with connection.cursor() as cursor:
-            for j in job_name:
-                cursor.execute(
-                    """INSERT INTO parser(job_name)
-                    VALUES(%s);""", (j.text,)
-                    )
-        with connection.cursor() as cursor:
-            for j in company_name:
-                cursor.execute(
-                    """INSERT INTO parser(company_name)
-                    VALUES(%s);""", (j.text,)
-                )
-        with connection.cursor() as cursor:
-            for j in description:
-                cursor.execute(
-                    """INSERT INTO parser(description)
-                    VALUES(%s);""", (j.text,)
+            cursor.execute(
+                """INSERT INTO parser(description, company_name, job_name)
+                VALUES(%s, %s, %s);""", (data[0], data[1], data[2], )
                 )
 
+        # with connection.cursor() as cursor:
+        #     for d in data:
+        #         cursor.execute(
+        #             """INSERT INTO parser(description, company_name, job_name)
+        #             VALUES(%s, %s, %s);""", d
+        #             )
+
         connection.commit()
-        print("[Table created successfully]")
+        print("[Data was successfully inserted into database]")
         connection.close()
     except Exception as _ex:
         print("[INFO] error while working with PostgreSQL", _ex)
